ece1c9a63dd8e95e54ed5c79525389e312a55898
keem02 <contradanza.90@gmail.com>
2025-02-12 08:36:51 +0900

    blog

--- blog/forms.py
+++ blog/forms.py
@@ -3,6 +3,9 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
+    title = forms.CharField()
+    content = forms.CharField(widget=forms.Textarea)
+
     class Meta:
         model = Post
         fields = ["title", "content"]
--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,18 @@ from .models import Post
 from django.db.models import Q
 
 
+# def post_list(request):
+#     posts = Post.objects.all().order_by("-created_at")
+#     return render(request, "blog/post_list.html", {"posts": posts})
 def post_list(request):
-    posts = Post.objects.all().order_by("-created_at")
+    query = request.GET.get("q")
+    if query:
+        posts = Post.objects.filter(
+            Q(title__icontains=query) | Q(content__icontains=query)
+        ).order_by("-created_at")
+    else:
+        posts = Post.objects.all().order_by("-created_at")
+
     return render(request, "blog/post_list.html", {"posts": posts})
 
 
@@ -43,15 +53,3 @@ def edit_post(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, "blog/edit_post.html", {"form": form, "post": post})
-
-
-def post_list(request):
-    query = request.GET.get("q")
-    if query:
-        posts = Post.objects.filter(
-            Q(title__icontains=query) | Q(content__icontains=query)
-        ).order_by("-created_at")
-    else:
-        posts = Post.objects.all().order_by("-created_at")
-
-    return render(request, "blog/post_list.html", {"posts": posts})
